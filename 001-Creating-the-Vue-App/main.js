92b21b1f766af48120d3296a03d09ccabcc1ed65
VasilyShat <kuremkarmerurk@gmail.com>
2026-05-31 22:05:33 +0200

    001-Added the completed challenge for the lesson.

--- 001-Creating-the-Vue-App/main.js
+++ 001-Creating-the-Vue-App/main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     // data: function () { --> data is a function that returns an object
     return {
       product: "Socks",
+      description:
+        "Premium socks with a modern logo design, offering comfort, durability, and everyday style.",
     };
   },
 });
