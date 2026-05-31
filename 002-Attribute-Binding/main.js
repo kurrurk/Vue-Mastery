7462c9182fd9f13a0e073cee3cb0f02fb9103bce
VasilyShat <kuremkarmerurk@gmail.com>
2026-05-31 22:32:20 +0200

    002-Attribute-Binding

---
+++ 002-Attribute-Binding/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      image_green: "./assets/images/socks_green.jpg",
+      image_blue: "./assets/images/socks_blue.jpg",
+      url_green: "#socks_green",
+      url_blue: "#socks_blue",
+    };
+  },
+});
