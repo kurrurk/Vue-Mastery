76798a6cfe94939ff5c40dca7ab55bb5fe2a8d8e
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-10 18:03:18 +0200

    025-Pinia Fundamentals: Creating our first store

---
+++ 025-Creating-our-first-store-with-Pinia/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue'
+</script>
+
+<template>
+  <todo-app />
+</template>
+
+<style scoped></style>
---
+++ 025-Creating-our-first-store-with-Pinia/src/stores/todoList.js
@@ -0,0 +1,11 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useTodoListStore = defineStore('todoList', () => {
+  // state
+  state: () => ({
+    todoList: [],
+  })
+  // getters
+  // actions
+})
