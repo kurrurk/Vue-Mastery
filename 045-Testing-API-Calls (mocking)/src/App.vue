bea70bcf6001cccf451da001e85f443399949ade
VasilyShat <kuremkarmerurk@gmail.com>
2026-06-21 17:36:27 +0200

    045-Testing API Calls (Mocking)

---
+++ 045-Testing-API-Calls (mocking)/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+import NotificationToast from "./components/NotificationToast.vue";
+
+const message = ref("Image uploaded successfully!");
+
+const clearNotification = () => {
+  message.value = "";
+};
+</script>
+
+<template>
+  <NotificationToast
+    status="success"
+    :message="message"
+    @clear-notification="clearNotification"
+  ></NotificationToast>
+</template>
+
+<style scoped>
+.notification {
+  max-width: calc(100vw - 30px);
+}
+</style>
